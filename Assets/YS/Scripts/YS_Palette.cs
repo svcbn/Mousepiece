26681ae4d96becccf3e142dc007b880e298fd68d
SeoYeong-su <86824098+SeoYeong-su@users.noreply.github.com>
2022-09-15 07:34:51 +0900

    220914 작업2

--- Assets/YS/Scripts/YS_Palette.cs
+++ Assets/YS/Scripts/YS_Palette.cs
@@ -9,30 +9,44 @@ public class YS_Palette : MonoBehaviour
     public Color color;
     Texture2D texture;
 
-    Vector3 colorPos;
+    Vector2 colorPos;
+
+    Vector2 paletteSize;
 
     // Start is called before the first frame update
     void Start()
     {
-        texture = (Texture2D)palette.mainTexture;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+        paletteSize = new Vector2(palette.GetComponent<RectTransform>().rect.width, palette.GetComponent<RectTransform>().rect.height);
+    }
 
-            if (Physics.Raycast(mouseRay, out hit))
-            {
-                if (hit.transform.name == palette.transform.name)
-                {
-                    colorPos = Input.mousePosition;
-                    color = texture.GetPixelBilinear(colorPos.x, colorPos.y);
-                }
-            }
-        }
+    public void SelectColor()
+    {
+        /*if (Input.GetMouseButtonDown(0))
+        {*/
+        //Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //RaycastHit hit;
+
+        //if (Physics.Raycast(mouseRay, out hit))
+        //{
+        //if (hit.transform.name == palette.transform.name)
+        //{
+        colorPos = transform.position;
+        Vector2 palettePos = palette.transform.position;
+
+        Vector2 pos = colorPos - palettePos + (paletteSize * 0.5f);
+        Vector2 normalizePos = new Vector2(pos.x / (paletteSize.x), pos.y / (paletteSize.y));
+
+        texture = palette.mainTexture as Texture2D;
+
+        color = texture.GetPixelBilinear(normalizePos.x, normalizePos.y);
+                //}
+            //}
+        //}
     }
 }
