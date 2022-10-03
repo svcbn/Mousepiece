51f4f71af5d6bfcf273ce3cde84b8f7554ddb5b9
SeoYeong-su <86824098+SeoYeong-su@users.noreply.github.com>
2022-10-04 08:21:34 +0900

    221004 - 캡쳐 기능, 캔버스 수정

---
+++ Assets/YS/Scripts/YS_Capture.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class YS_Capture : MonoBehaviour
+{
+    string name = "capture";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Shot();
+        }
+    }
+
+    void Shot()
+    {
+        // Ä¸ÃÄ (Å¸°Ù¸¸)
+        RenderTexture renderTex = GetComponent<Camera>().targetTexture;
+        Texture2D tex = new Texture2D(renderTex.width, renderTex.height, TextureFormat.RGBA32, false);
+        RenderTexture.active = renderTex;
+        tex.ReadPixels(new Rect(0, 0, renderTex.width, renderTex.height), 0, 0);
+        tex.Apply();
+
+        // ÆÄÀÏ ÀúÀå
+        File.WriteAllBytes($"{Application.dataPath}/{name}.png", tex.EncodeToPNG());
+    }
+}
